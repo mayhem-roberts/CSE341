4001cd23e627491ffacae21e044321f098100601
Meagan Roberts <reaganmoberts@gmail.com>
2023-01-07 17:04:00 -0700

    edit routes for test

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayName = (req, res) => {
-    const data =
-      'Meagan Roberts';
-    res.status(200).send(data);
-  };
-  
-  module.exports = {
-    displayName,
-  };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const routes = require('express').Router();
-const con = require('../controllers/');
 
-routes.get('/', con.displayName);
+routes.get('/', (req, res) => {
+    res.send('Meagan Roberts')
+});
+routes.get('/test', (req, res) => {
+    res.send('Emily DeCato')
+}); 
 
 module.exports = routes;
