634e471be6d798fda2d54c14b39d63e08007bb34
Meagan Roberts <reaganmoberts@gmail.com>
2023-01-14 18:02:45 -0700

    project01 folder

---
+++ project01/controllers/contacts.js
@@ -0,0 +1,9 @@
+// route for getting all contacts
+const getContacts = async(req, res, next) => {
+
+}
+
+// route for getting one contact 
+const getContact = async(req, res, next) => {
+    
+}
--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const  fun= require('../controllers/');
-
-routes.get('/', fun.myName);
-routes.get('/test', fun.familyName);
-
-module.exports = routes;
