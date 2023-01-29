f2a2493d77d8539eabbd91b97c3f0cd12aa689b6
Meagan Roberts <reaganmoberts@gmail.com>
2023-01-28 18:31:33 -0700

    week04 - swagger

--- project01/routes/index.js
+++ project01/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
+router.use('/', require('./swagger'));
+router.use('/contacts', require('./contacts'));
 
 module.exports = router;
 
